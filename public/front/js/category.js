3b68694ca1770623817d8d094aafbba9a9d2c8e8
wrgyj <1438906047@qq.com>
2018-05-14 21:14:50 +0800

    搜索页完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -10,6 +10,26 @@ $(function(){
     success: function(info){
       var htmlStr = template('leftTpl',info);
       $('.lt_category_left ul').html(htmlStr);
+      renderSecondById(info.rows[0].id);
     }
   })
+  
+  $('.lt_category_left ul').on('click','a',function(){
+    var id = $(this).data('id');
+    renderSecondById(id);
+    $(this).addClass('current').parent().siblings().find('a').removeClass('current');
+  })
+  
+  function renderSecondById(id){
+    $.ajax({
+      type:'get',
+      url:'/category/querySecondCategory',
+      data:{id:id},
+      success:function(info){
+        // console.log(info);
+        var htmlStr = template('rightTpl',info);
+        $('.lt_category_right ul').html(htmlStr);
+      }
+    })
+  }
 })
--- public/front2/js/common.js
+++ public/front2/js/common.js
@@ -3,6 +3,7 @@
  */
 
 
+// 可以防止变量全局污染
 $(function() {
 
   // 区域滚动初始化
@@ -17,4 +18,32 @@ $(function() {
     interval:1000 //自动轮播周期，若为0则不自动播放，默认为0；
   });
 
-})
+});
+
+// 作用: 根据传入的key值, 解析地址栏参数, 获取对应的value值
+function getSearch( key ) {
+  // 获取地址栏参数  "?name=pp&age=18&desc=%E5%B8%85"
+  var search = location.search;
+  // 需要解码  "?name=pp&age=18&desc=帅"
+  search = decodeURI( search );
+  // 去掉 ?
+  search = search.slice(1);
+  // 根据 & 将字符串, 切割成数组   ["name=pp", "age=18", "desc=帅"]
+  var arr = search.split("&");
+
+  var obj = {};
+
+  arr.forEach(function( element, index ) {
+    // "name=pp"
+    var k = element.split("=")[0];
+    var v = element.split("=")[1];
+    // obj["name"]="pp";
+    obj[ k ] = v;
+  });
+
+  // return obj["name"];
+  return obj[ key ];
+}
+
+
+
